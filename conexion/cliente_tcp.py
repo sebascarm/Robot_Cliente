900bb4076b6559b54a633a7ec12cfd7c08eac8a1
Sebastian Carmona <sebascarm@gmail.com>
2020-01-27 23:52:52 -0300

    cliente tcp con recepcion de binarios

--- conexion/cliente_tcp.py
+++ conexion/cliente_tcp.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 ############################################################
-### CLIENTE TCP VERSION 2.2                              ###
+### CLIENTE TCP VERSION 3.0                              ###
 ############################################################
 ### ULTIMA MODIFICACION DOCUMENTADA                      ###
-### 24/09/2019                                           ###
+### 27/01/2020                                           ###
+### Reduccion de codigo                                  ###
+### Posibilidad de recibirdatos binarios                 ###
+### Espera de 0.1 inecesaria, lectura bloqueante         ###
 ### Espera de 0.1 para escuchar mensajes entrantes       ###
 ### timeout en socket para no bloquear proceso al cerrar ###
 ### Uso de ThreadAdmin                                   ###
@@ -15,86 +18,145 @@
 import socket
 import time
 from thread_admin import ThreadAdmin
-
+import cv2
 
 class Cliente_TCP(object):
     def __init__(self):
         self.soc = ""
-        self.conexion     = False
-        self.th_conexion  = ''
-        self.th_recepcion = ''      #hilo de recepcion
-        
-    def config(self, Host, Puerto, Callback, Buffer = 1024):
-        self.HOST       = Host
-        self.PUERTO     = int(Puerto)
-        #self.recepcion  = Cola_Recepcion
+        self.conexion       = False             # Estado de la conexion
+        self.estado         = 0                 # Valor de utlimo estado
+        self.th_conexion    = ThreadAdmin()
+        self.th_mensajes    = ThreadAdmin()
+        #self.th_recepcion  = ''     # hilo de recepcion
+        self.host           = ''
+        self.puerto         = ''
+        self.buffer         = 1024
+        self.callback       = ''
+        self.binario        = False
+
+    def config(self, Host, Puerto, Callback, Buffer = 1024, Binario=False):
+        self.host       = Host
+        self.puerto     = int(Puerto)
         self.buffer     = Buffer
-        self.callback   = Callback      #Funcion de rellamada de estados
+        self.binario    = Binario       # Solo puede recibir binario, envia texto
+        self.callback   = Callback      # Funcion de rellamada de estados
         #Mensajes de callback: -1 Error 0 Desconectado 1 Conectando 2 Conectado
         #                       3 Envio de Datos 4 Recepcion de Datos
         #Calback funcion ej: calback(codigo, mensaje): / calback(self, codigo, mensaje): /
 
     def conectar(self):
-        #abrimos hilo de recepcion
+        # control de mensajes si ya se encuentra en ejecucion
+        if not self.th_cola.state:
+            self.th_cola.start(self.__th_mensajes,'','MENSAJES-TCP', 3, self.callback)
         if not self.conexion:
-            #abrimos un hilo nuevo
-            self.th_conexion = ThreadAdmin(self.__interno_conectar,'','CONEXION-TCP', 10)
+            self.th_conexion.start(self.__th_conectar,'','CONEXION-TCP', 10)
         else:
-            self.callback(-1, "Conexion actualmente establecida")
+            self.__estado(-1, "Conexion actualmente establecida")
+            
 
-    def __interno_conectar(self):
+    def __th_conectar(self):
         try:
             self.conexion = True
-            self.callback(1, "Estableciendo conexion")
+            self.__estado(1, "Estableciendo conexion")
             self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.soc.settimeout(3)
-            conectar = self.soc.connect((self.HOST, self.PUERTO)) #si tiene que usarse
-            self.callback(2, "Conexion establecida")
-            self.__interno_recibir()
+            conectar = self.soc.connect((self.host, self.puerto)) #si tiene que usarse
+            self.__estado(2, "Conexion establecida")
+            self.__recibir()
         except Exception:
             self.conexion = False
-            self.callback(-1, "Error en conexion")
+            self.__estado(-1, "Error en conexion")
 
     def enviar(self, mensaje):
         if self.conexion:
             try:
                 self.soc.sendall(mensaje.encode('utf-8'))
-                self.callback(3, "Enviado: " + mensaje)
+                self.__estado(3, "Enviado: " + mensaje)
             except:
                 self.desconectar()
-                self.callback(-1, "Problemas al enviar datos \n  Conexion Cerrada")
+                self.__estado(-1, "Problemas al enviar datos.  Conexion Cerrada")
         else:
             try:
-                self.callback(-1, "Sin Conexion: " + mensaje)
+                self.__estado(-1, "Sin Conexion: " + mensaje)
             except:
                 print("Sin Conexion: " + mensaje)
 
-    def __interno_recibir(self):
+    def __recibir(self):
+        self.soc.settimeout(3) #time out de escucha // posiblementa innecesario
         while self.conexion:
             try:
-                #leer del puerto
                 recibido = self.soc.recv(self.buffer)    #leer del puerto - posible bloqueo hasta recepcion (con timeout no hay)
                 if recibido == b'':
-                    self.callback(-1, "Servidor Desconectado")
+                    self.__estado(-1, "Servidor Desconectado")
                     self.desconectar()
                 else:
-                    #Recepcion de datos
-                    self.callback(4, recibido.decode())
+                    self.__estado(4, recibido.decode()) # Recepcion de datos
             except socket.timeout as err:
                 pass    # time out continua con el loop
             except Exception as err:
-                self.callback(-1, "Error SOC: " + str(err))
+                self.__estado(-1, "Error SOC: " + str(err))
                 self.desconectar()
-            # Espera antes de leer nuevamente el puerto
-            time.sleep(0.1)
+            # Espera antes de leer nuevamente el puerto // elimanda la espera ya que la lectura es bloqueante
+            # time.sleep(0.1)
+
+    # recepcion binaria
+    def __recibir_bin(self):        
+        self.soc.settimeout(3) #time out de escucha // posiblementa innecesario
+        payload_size = struct.calcsize("H")
+        print (payload_size)
+        while self.conexion:
+            while len(recibido) < payload_size:
+                try:
+                    recibido += self.soc.recv(self.buffer)  # leer del puerto - posible bloqueo hasta recepcion (con timeout no hay)
+                    if recibido == b'':
+                        self.__estado(-1, "Servidor Desconectado")
+                        self.desconectar()
+                    else:
+                        # correcto
+                        packed_msg_size = recibido[:payload_size]
+                        recibido = recibido[payload_size:]
+                        msg_size = struct.unpack("H", packed_msg_size)[0]
+                        while len(recibido) < msg_size:
+                            recibido += self.soc.recv(self.buffer)  # leer del puerto - posible bloqueo hasta recepcion (con timeout no hay)
+                        frame_data = recibido[:msg_size]
+                        recibido = recibido[msg_size:]
+                        ###
+                        frame=pickle.loads(frame_data)
+                        print (frame)
+                        cv2.imshow('frame',frame)
+
+
+                except socket.timeout as err:
+                    pass    # time out continua con el loop
+                except Exception as err:
+                    self.__estado(-1, "Error SOC: " + str(err))
+                    self.desconectar()
             
+
+            
+
+
         
     def desconectar(self):
         try:
             self.conexion = False
             self.th_conexion.close()
             self.soc.close()
-            self.callback(0, "Conexion Cerrada")
+            self.__estado(0, "Conexion Cerrada")
         except Exception:
-            self.callback(-1, "Error al cerrar conexion")
+            self.__estado(-1, "Error al cerrar conexion")
 
+    # codificacion y envio de estados y mensaje
+    def __estado(self, Estado, Mensaje):
+        self.estado = Estado
+        self.cola_codigo.put(Estado)
+        self.cola_mensaje.put(Mensaje)
+
+    # loop de lectura de mensajes
+    def __th_mensajes(self):
+        while self.conexion:
+            if self.cola_mensaje.qsize() > 0:
+                codigo  = self.cola_codigo.get()
+                mensaje = self.cola_mensaje.get()
+                self.callback(codigo, mensaje)
+            time.sleep(self.mensaje_velocidad)
